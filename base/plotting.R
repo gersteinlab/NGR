ba98ff426a1279d46e5ee997a913d7b1955cea4c
Hussein Mohsen <husseinmohsen@Husseins-MacBook-Pro.local>
2019-07-17 14:32:00 -0400

    Phase I analysis part added.

--- base/plotting.R
+++ base/plotting.R
@@ -1,9 +1,10 @@
 # plot histograms of gene and pathway distributions
-generate_histogram_from_list <- function(input_list){
+generate_histogram_from_list <- function(input_list, x_label='', title='', bins=30){
   df <- data.frame(values=input_list)
   p <-  ggplot(df, aes(x=values)) +
-    geom_histogram(color='black', fill='#ff5252') + 
-    theme(panel.grid.major=element_blank(), panel.grid.minor=element_blank(), panel.background=element_blank(), axis.line=element_line(), text=element_text(family='Trebuchet MS', face='bold'), axis.title=element_text(size=12), axis.text=element_text(size=13), plot.title=element_text(size = 15, hjust=0.5)) + ylab('Frequency')
+    geom_histogram(color='black', fill='#ff5252', bins=bins) + 
+    ggtitle(title) +
+    theme(panel.grid.major=element_blank(), panel.grid.minor=element_blank(), panel.background=element_blank(), plot.background=element_rect(fill='white'), axis.line=element_line(), text=element_text(family='Trebuchet MS', face='bold'), axis.title=element_text(size=12), axis.text=element_text(size=13), plot.title=element_text(size = 15, hjust=0.5)) + ylab('Frequency') + xlab(x_label)
   
   return(p)
 }
--- tcga/code/tcga-diff_exp.R
+++ tcga/code/tcga-diff_exp.R
@@ -109,7 +109,7 @@ for(i in 1:length(cancer_types)){
     save.image(paste(cancer_types[i], "checkpoint5.RData", sep="_"))
 
     gene_table <- display_diff_exp_stats(lrt)
-    write.table(gene_table[, c("genes", "logFC", "FDR", "PValue")], paste(cancer_types[i], "expression", "results.csv", sep="_"), row.names=F, col.names=T, quote=F)
+    write.table(gene_table[, c("genes", "logFC", "FDR", "PValue")], paste(cancer_types[i], "expression", "results.csv", sep="_"), row.names=F, col.names=T, quote=F, sep=',')
   }, error = function(e) {
     print(paste("Error with ", cancer_types[i], sep=""))
     print(e)
