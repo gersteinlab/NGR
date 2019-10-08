4132d026c012e8eb533e19526166c7a05eec1467
Hussein Mohsen <husseinmohsen@vpn172022146161.its.yale.internal>
2019-10-07 20:38:57 -0400

    Updated feature merging script.

--- base/merge_features.R
+++ base/merge_features.R
@@ -9,25 +9,40 @@
 # 
 # print(argv)
 
+source('plotting.R')
+
 library(dplyr)
+library(scales)
 
-cancer_type = "" # default is empty string and implies pancancer analysis
-if(cancer_type == ""){
-  feature_files_filename <- paste("feature_file_lists/", cancer_type, "feature_files.txt", sep="")
-} else
-  feature_files_filename <- paste("feature_file_lists/", cancer_type, "_feature_files.txt", sep="")
-
+feature_files_dir <- "feature_file_lists/"
+feature_files_basename <- "somatic_MC3_feature_files"
+filename <- paste(feature_files_dir, cancer_type, feature_files_basename, ".txt", sep="")
 feature_files <- as.character(read.table(feature_files_filename)[, 1]) # read the list of files
 
 # All feature files have: gene names in 1st column, feature value of interest in 2nd
 feature_data_combined <- read.table(feature_files[1], header=T, sep=",")[, 1:2]
 print(paste("Reading", feature_files[1], "Done.", sep=" "))
-	
+
 for(i in 2:length(feature_files)){
-	feature_data <- read.table(feature_files[i], header=T, sep=",")
-	feature_data_combined <- full_join(feature_data_combined, feature_data[, 1:2]) # merge files
-	print(paste("Processing", feature_files[i], "Done.", sep=" "))
+  feature_data <- read.table(feature_files[i], header=T, sep=",")
+  feature_data_combined <- full_join(feature_data_combined, feature_data[, 1:2]) # merge files
+  print(paste("Processing", feature_files[i], "Done.", sep=" "))
 }
 
 print(head(feature_data_combined))
 print(dim(feature_data_combined))
+
+feature_data_combined[is.na(feature_data_combined)] <- 0 # replace NAs with 0's
+feature_data_combined[, 2:ncol(feature_data_combined)] <- apply(feature_data_combined[, 2:ncol(feature_data_combined)], 2, rescale) # min-max scaling per column
+feature_data_combined$combined_score <- apply(feature_data_combined[2:ncol(feature_data_combined)], 1, mean) # calculate combined scores
+feature_data_combined <- feature_data_combined[order(feature_data_combined$combined_score, decreasing=TRUE), ] # sort by combined score
+
+print("Top genes:")
+head(feature_data_combined[, c("gene", "combined_score")], 10)
+
+print("Bottom genes:")
+tail(feature_data_combined[, c("gene", "combined_score")], 10)
+
+save(feature_data_combined, file=paste("combined_scores_", feature_files_basename, ".RData", sep=""))
+
+generate_histogram_from_list(feature_data_combined$combined_score, x_label='NGR Combined Score', title='', bins=10)
--- gene_lists/README.txt
+++ gene_lists/README.txt
@@ -1,7 +1,7 @@
 # To generate dependency score files, locally run code/generate_gene_dependency_lists.Rmd locally.
 
 # To generate indicator variable feature score files (i.e. indicator variables), locally run the commands below in known_genes/:
-awk -F "," 'BEGIN{print "gene,_COSMIC_v90_membership"} {print$1",1"}' COSMIC_v90_Census_all_Oct_1_19.csv > COSMIC_v90_membership.csv
-awk -F "," 'BEGIN{print "gene,Vo_list_membership"} {if(NR>2 && index($1, "classif") == 0 && index($1, "*") == 0){print$1",1"}}' Vogelstein_et_al_list_Jul_15_19_S2A.csv > Vogelstein_list_membership.csv
+awk -F "," 'BEGIN{print "gene,list_membership_COSMIC_v90”} {print$1",1"}' COSMIC_v90_Census_all_Oct_1_19.csv > COSMIC_v90_membership.csv
+awk -F "," 'BEGIN{print "gene,list_membership_Vo”} {if(NR>2 && index($1, "classif") == 0 && index($1, "*") == 0){print$1",1"}}' Vogelstein_et_al_list_Jul_15_19_S2A.csv > Vogelstein_list_membership.csv
 awk -F "," '{if(NR>2 && index($1, "classif") == 0 && index($1, "*") == 0){print$1",1"}}' Vogelstein_et_al_list_Jul_15_19_S2B.csv >> Vogelstein_list_membership.csv
-awk 'BEGIN{print "gene,Fr_list_membership"} {for(i=1; i<=NF; i++){print $i",1"}}' Frampton_et_al_list_Oct_1_19.csv > Frampton_list_membership.csv
+awk 'BEGIN{print "gene,list_membership_Fr”} {for(i=1; i<=NF; i++){print $i",1"}}' Frampton_et_al_list_Oct_1_19.csv > Frampton_list_membership.csv
