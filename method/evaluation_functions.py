Bug fix in mobility calculation and updates in a few scripts

method/evaluation_functions.py
@@ -2,7 +2,7 @@ import numpy as np
 import scipy.stats as st
 from scipy.spatial import distance
 from sklearn.metrics import roc_auc_score, f1_score, accuracy_score, precision_score, recall_score, precision_recall_curve, auc
-from scipy.stats import hypergeom
+from scipy.stats import hypergeom, rankdata
 
 import helper_functions as hp
 from scipy.constants.codata import precision
@@ -92,9 +92,9 @@ def compare_lists(ranked_score_lists, gold_standard_file, list_names, cancer_typ
             elif cm == 'Hypergeom_test': # hypergeometric enrichment test
                 known_genes_file = '../gene_lists/known_genes/COSMIC_v90_membership.csv'
                 known_genes = np.genfromtxt(known_genes_file, delimiter=',', skip_header=1, dtype='U25,i4', names=('gene','score'))
-                
-                S = 1000 #int(0.025 * N) # sample size
-                
+
+                S = int(0.1 * N) #int(0.025 * N) # sample size
+ 
                 sample_common_known_genes = np.intersect1d(known_genes['gene'], ranked_scores[id_colname][0:S])
                 x = len(sample_common_known_genes) # number of "successes" in the sample of size N
 
@@ -113,7 +113,7 @@ def compare_lists(ranked_score_lists, gold_standard_file, list_names, cancer_typ
 
 ## EVALUATION FUNCTIONS:
 
-# MOBILITY LIST FUNCITONS
+# MOBILITY LIST FUNCTIONS
 # generated mobility lists across variant types, cancer types, ppis and ppi_levels
 def generate_batch_gene_mobility_lists(uids_filename):
     matrices_dir = 'results/score_matrices/'
@@ -152,16 +152,9 @@ def generate_batch_gene_mobility_lists(uids_filename):
 # generates lists sorted in increasing order of mobility: difference between initial and post-diffusion rank
 # values are -ve (for upward mobility) and +ve (downward); upward 'passenger' genes are of more interest
 def generate_gene_mobility_list(initial_matrix, ngr_matrix, gene_index):
-    initial_scores_order = np.flip(np.argsort(np.mean(initial_matrix, axis=1)))
-    ngr_scores_order = np.flip(np.argsort(np.mean(ngr_matrix, axis=1)))
-
-    initial_ranks = np.zeros(initial_scores_order.shape)
-    ngr_ranks = np.zeros(ngr_scores_order.shape)
+    initial_ranks = rankdata(-np.mean(initial_matrix, axis=1), method='min')
+    ngr_ranks = rankdata(-np.mean(ngr_matrix, axis=1), method='min')
     
-    for i in range(len(initial_scores_order)):
-        initial_ranks[initial_scores_order[i]] = i # rank of each gene in a would-be sorted list
-        ngr_ranks[ngr_scores_order[i]] = i
-        
     mobility_score = initial_ranks - ngr_ranks # mobility is the different in ranks between initial and ngr list
     
     mobility_list = np.array(list(zip(gene_index, mobility_score, initial_ranks, ngr_ranks)), dtype=[('gene', 'U25'), ('mobility_score', 'i4'), ('initial_rank', 'i4'), ('ngr_rank', 'i4')])

method/ngr.py
@@ -4,39 +4,29 @@ import evaluation_functions as eval
 import numpy as np
 import datetime
 
+# If revisited: 
+# Might want to handle ties in terms of same initial score: i.e. assign same initial score the same rank and adjust T beta value accordingly
+# in pushed code, initial and ngr/umg ranks start at 1 (hence +1 to initial_ranks and postprop_ranks in calculate_mobiltiy()), in current code used to generate paper results it starts at 0 (hence no +1 in generate_gene_mobility_lists())
+
 # Y is the vector of initial scores
 # W is the weight matrix, i.e. PPI network
 # W_index is the ordered list of genes as they appear in cols and rows
-def run(X, W, alpha=0.8):
+def run(M, W, alpha=0.8):
     print('\nNGR diffusion starts...\n')
-    n_iterations = 450; i = 0; 
-    X0 = X; X_total_heat = 0; heat_diff=0.00001
-    
-    while i < n_iterations and heat_diff > 0:
-        X = (alpha * np.dot(W, X)) + ((1 - alpha) * X0)
+    i = 0; epsilon = 1 / np.power(10, 6); max_iterations = 350
+    M0 = M; M_delta = epsilon
+
+    print('{0}\nPropagation in progress...'.format(datetime.datetime.now()))
+    while M_delta >= epsilon and i < max_iterations:
+        previous = M
+
+        M = (alpha * np.dot(W, M)) + ((1 - alpha) * M0) # propagation step      
+        M_delta = np.linalg.norm(M - previous)
+        print('\tIter. {0} complete. Heat difference: {1}'.format(i+1, M_delta))
         
-        #Sm = (W > 0) * Sm # Step 1: W-based ReLU on S                
-        #Sm = np.apply_along_axis(hp.nonzero_softmax, axis=0, arr=Sm, inverted_x=True) # Step 2: row-wise softmax
-        #S = hp.totality_scale(S) # re-normalize after each step; safety step 
-
-        X_total_heat_prev = X_total_heat
-        X_total_heat = np.sum(X)
-        heat_diff = np.abs(X_total_heat - X_total_heat_prev)
-
-        #print('Scores at end of iteration {0}: {1}'.format(i, S.T))
-        
-        # To remove this final block and the initial_scores parameter in function header in the final code
-        #gold_standard_file = '../gene_lists/code/results/cancerMine_both_depmap_both_values_mean_scores_all_tissue.csv'
-        #ngr_list, initial_scores = hp.process_ngr_results(initial_scores, S.reshape(S.shape[0], ), save_files=False) # ngr and initial scores in both structured arrays (i.e. called lists here) are re-ordered w.r.t. to their respective decreasing order of scores
-        #eval.evaluate_results((initial_scores, ngr_list), gold_standard_file, ('Initial', 'NGR'),  N=3000, comp_measures=('AO', 'weighted_tau', 'manhattan_dist', 'tau', 'RBO', 'euclidean_dist'))
-
-        print('\tIter. {0} complete. Heat difference: {1}'.format(i+1, heat_diff))
         i += 1
 
-    # start here: in-place, 0-based softmax,
-    print('\nNGR diffusion done.')
+    print('Propagation done: {0} iterations.'.format(i))
     print(datetime.datetime.now())
 
-    print(X)
-    
-    return X
+    return M